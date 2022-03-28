c676aa500f864a375949bd86d03ba37352b17e1a
dibss <ludovicad01@gmail.com>
2022-03-28 20:11:58 +0200

    .

---
+++ jsnack3/main.js
@@ -0,0 +1,12 @@
+// dato l'aaray di nomi e dati due numeri min e max (min più piccolo di max).
+// Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
+// Eseguiamo questo esercizio prima con forEach e poi con filter.
+
+const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+
+const min = 1;
+const max = 5;
+let newArray = [];
+
+console.log(minMax);
+
